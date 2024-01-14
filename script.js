8078ae415940410f0c1fcd3f6f14275cce54f4c7
Abiolayoung <abiolayoung229@gmail.com>
2024-01-14 21:29:57 +0100

    update

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // array provide methods to manage the order of elements
 // 2 ways of creating array in javascript
 // let arr = new Array() // method 1
-let arr2 = []; //method 2
+// let arr2 = []; //method 2
 
-let fruits = ["apple", "orange", "plum"]
+// let fruits = ["apple", "orange", "plum"]
 // array elements are numbered starting from index 0
 
 // alert(fruits[0]) // this will print out the apple starting from the count of 0
@@ -40,9 +40,49 @@ let fruits = ["apple", "orange", "plum"]
 // alert(fruits.at(-1)) // 
 
 // array methods
-// push method- push appends or add and element to the end while,
+// pop, push, shift, unshift
+// push method- push appends or add and element to the end while, pop remove an element from the end
 // shift- remove and element from the beginner
 
+let fruits = ["apple", "orange", "plum"]
+// fruits.pop() //this will pop out plum which is the last item of the array
+// fruits.push("pear") // push will add a value to the end of the array
+// alert(fruits)
+
+fruits.shift() // shift will remove the first item of the array
+fruits.unshift("pear", "lemom", "grape") // unshift will add to the first of the array, you can also add multiple items by 
+// specifying the items in the parenthesses
+// alert(fruits)
+
+// Arrays are can be referenced e,g
+// let arr = fruits;
+// alert(arr===fruits)
+// arr.push("apple")
+// alert(arr)
+
+// the shift and unshift method move the array elements to either left or right , but pop and push only remove and add to the end of the array and not move their index
+
+// how to loop over the indexes of an array
+let arr = ["apple", "orange", "plum"];
+
+// for(let i=0; i<arr.length; i++){
+//     alert(arr[i])
+// }
+
+// using for off loop - for off loop is recommended
+// for(let fruit of arr){
+//     alert(fruit)
+// }
+
+// using for in loop - used for generic objects
+for(let key in arr){
+    alert(arr[key])
+}
+
+let matrix = [
+    [1,2,3], [4,5,6], [7,8,9]
+]
+alert(matrix[3][1])
 
 
 
@@ -61,4 +101,6 @@ upperCaseNames
 // console.log(capitalize(abiola));
 
 // map will apply a callback function to each value of the array
+
+// how to target the value of an array place inside of an arry
 
